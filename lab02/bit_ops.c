782527761a331072398c177be35e72dff2660717
gatsby <gatsby.junce@gmail.com>
2022-04-18 17:13:27 +0800

    Finished lab02-exercise1

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -3,17 +3,41 @@
 
 /* Returns the Nth bit of X. Assumes 0 <= N <= 31. */
 unsigned get_bit(unsigned x, unsigned n) {
-    /* YOUR CODE HERE */
-    return -1; /* UPDATE WITH THE CORRECT RETURN VALUE*/
+    /*
+                :       n
+    x           :   abababab
+                           n
+    x >> n      :   ???ababa
+
+    1           :   00000001
+    x >> n & 1  :   0000000a
+    */
+    return x >> n & 1;
 }
 
 /* Set the nth bit of the value of x to v. Assumes 0 <= N <= 31, and V is 0 or 1 */
 void set_bit(unsigned *x, unsigned n, unsigned v) {
-    /* YOUR CODE HERE */
+    /*
+                :       n
+    1 << n      :   00001000
+    ~(1 << n)   :   11110111
+    x &= ~(<<)  :   aaaa0aaa
+
+    (v << n)    :   0000v000
+    x |= (<<)   :   aaaavaaa
+    */
+   *x = (*x & ~(1 << n)) | (v << n);
 }
 
 /* Flips the Nth bit in X. Assumes 0 <= N <= 31.*/
 void flip_bit(unsigned *x, unsigned n) {
-    /* YOUR CODE HERE */
+    /*
+                            n
+    x:              :   xxxxvxxx
+    1 << n          :   00001000
+
+    x ^ (1 << n)    :   xxxx^xxx
+    */
+    *x ^= (1 << n);
 }
 
